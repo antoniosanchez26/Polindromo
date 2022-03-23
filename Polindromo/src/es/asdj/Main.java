c6819bbed0ca5cd414c5b73b5b8220ae56680b32
al036991 <al036991@ieslaencanta.com>
2022-03-23 09:50:10 +0100

    terminado

--- Polindromo/src/es/asdj/Main.java
+++ Polindromo/src/es/asdj/Main.java
@@ -6,32 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        String texto,texto2 = "",textosinespacios = "";
+        String texto, textoAlReves = "", textoSinEspacios = "";
 
         System.out.print("Escribe una frase: ");
         texto = entrada.nextLine();
         System.out.println();
 
-        for (int i = texto.length() -1; i >= 0 ; i--) {
+        for (int i = texto.length() - 1; i >= 0 ; i--) {
             if (texto.charAt(i) != ' ') {
-                texto2 += texto.charAt(i);
+                textoAlReves += texto.toLowerCase().charAt(i);
             }
         }
 
-        for (int i = 0; i > texto.length();i++) {
+        for (int i = 0; i < texto.length(); i++) {
             if (texto.charAt(i) != ' ') {
-                textosinespacios += texto.charAt(i);
+                textoSinEspacios += texto.toLowerCase().charAt(i);
             }
         }
 
-        if (texto.equalsIgnoreCase(texto2)) {
+        if (textoSinEspacios.equals(textoAlReves)) {
             System.out.print("El texto es un políndromo");
         } else {
             System.out.print("El texto no es un políndromo");
         }
         System.out.println();
-        for (int i = 0; i < texto.length();i++) {
-            System.out.print(textosinespacios.charAt(i));
-        }
     }
 }
